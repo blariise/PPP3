e1c236919869a871191659384a4d2bc5107404c1
blarrise <61951559+blariise@users.noreply.github.com>
2025-02-25 01:50:09 +0100

    drill #2

--- 3/main.cpp
+++ 3/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 int main() {
   std::vector<int> numbers{};
@@ -7,10 +8,9 @@ int main() {
   while (std::cin >> input_number) {
     numbers.push_back(input_number);
     if (std::size(numbers) == 2) {
-      for (const auto num : numbers) {
-        std::cout << num << " ";
-      }
-      std::cout << '\n';
+      std::ranges::sort(numbers);
+      std::cout << "the smaller value is: " << numbers[0] << '\n';
+      std::cout << "the larger value is: " << numbers[1] << '\n';
     }
   }
   return 0;
