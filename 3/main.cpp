d4e96f383b5da33f1e38dbb88fb000d815bb7d11
blarrise <blariise1337@gmail.com>
2025-02-25 01:24:06 +0100

    drill #1

--- 3/main.cpp
+++ 3/main.cpp
@@ -1,3 +1,17 @@
+#include <iostream>
+#include <vector>
+
 int main() {
+  std::vector<int> numbers{};
+  int input_number {};
+  while (std::cin >> input_number) {
+    numbers.push_back(input_number);
+    if (std::size(numbers) == 2) {
+      for (const auto num : numbers) {
+        std::cout << num << " ";
+      }
+      std::cout << '\n';
+    }
+  }
   return 0;
 }
