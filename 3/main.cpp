27783eaaf208400152529987f35344b102cbfa94
blarrise <61951559+blariise@users.noreply.github.com>
2025-02-26 14:55:09 +0100

    drill #7 #8

--- 3/main.cpp
+++ 3/main.cpp
@@ -4,21 +4,35 @@
 int main() {
   std::vector<double> numbers{};
   double input_number {};
+  std::string unit {};
   double largest {};
   double smallest {};
   bool is_first_iter { true };
-  while (std::cin >> input_number) {
+  while (std::cin >> input_number >> unit) { 
+    double converted_value {};
+    if (unit == "cm") {
+      converted_value = input_number;
+    } else if (unit == "m") {
+      converted_value = input_number * 100;
+    } else if (unit == "in") {
+      converted_value = input_number * 2.54;
+    } else if (unit == "ft") {
+      converted_value = input_number * 30.48;
+    } else {
+      continue;
+    }
+
     if (is_first_iter) {
-      smallest = input_number;
-      largest = input_number;
+      smallest = converted_value;
+      largest = converted_value;
       is_first_iter = false;
       continue;
     } else {
-      if (input_number < smallest) {
-        smallest = input_number;
+      if (converted_value < smallest) {
+        smallest = converted_value;
         std::cout << "the smallest so far\n";
-      } else if  (input_number > largest) {
-        largest = input_number;
+      } else if  (converted_value > largest) {
+        largest = converted_value;
         std::cout << "the largest so far\n";
       }
     }
