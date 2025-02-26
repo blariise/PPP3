845ac1af539951844d5fbbb57078a0f6415bbced
blarrise <61951559+blariise@users.noreply.github.com>
2025-02-26 15:13:59 +0100

    drill #9

--- 3/main.cpp
+++ 3/main.cpp
@@ -8,20 +8,24 @@ int main() {
   double largest {};
   double smallest {};
   bool is_first_iter { true };
+  int count_numbers { 0 };
+  double sum_numbers { 0 };
   while (std::cin >> input_number >> unit) { 
     double converted_value {};
     if (unit == "cm") {
-      converted_value = input_number;
+      converted_value = input_number / 100;
     } else if (unit == "m") {
-      converted_value = input_number * 100;
+      converted_value = input_number;
     } else if (unit == "in") {
-      converted_value = input_number * 2.54;
+      converted_value = input_number * 0.0254;
     } else if (unit == "ft") {
-      converted_value = input_number * 30.48;
+      converted_value = input_number * 0.3048;
     } else {
       continue;
     }
 
+    ++count_numbers;
+    sum_numbers += converted_value;
     if (is_first_iter) {
       smallest = converted_value;
       largest = converted_value;
@@ -37,5 +41,9 @@ int main() {
       }
     }
   }
+  std::cout << "smallest number(m): " << smallest << '\n';
+  std::cout << "largest number(m): " << largest << '\n';
+  std::cout << "numbers count: " << count_numbers << '\n';
+  std::cout << "sum of numbers(m): " << sum_numbers << '\n';
   return 0;
 }
