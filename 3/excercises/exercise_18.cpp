5e77894050ca43cbefa836eb0229212e20b67602
blarrise <61951559+blariise@users.noreply.github.com>
2025-03-01 01:20:37 +0100

    exercises #19 #20

--- 3/excercises/exercise_18.cpp
+++ 3/excercises/exercise_18.cpp
@@ -7,11 +7,14 @@ int main() {
   std::string name {};
   int score {};
   bool name_twice {};
+
+  std::cout << "Enter name and score ex. Simon 1337:\n";
   while (std::cin >> name >> score) {
     name_twice = false;
-    if (name == "NoName" && score == 0) {
+
+    if (name == "NoName" && score == 0)
       break;
-    }
+
     for (const auto& n : names) {
       if (name == n) {
         std::cout << "You entered name twice!\n";
@@ -24,9 +27,41 @@ int main() {
       scores.push_back(score);
     }
   }
+  
+  bool isName {};
+  std::cout << "Enter names to find scores:\n"; 
+  while (std::cin >> name) {
+    isName = false;
 
-  for (std::size_t i { 0 }; i < std::size(names); ++i) {
-    std::cout << '(' << names[i] << ',' << scores[i] << ")\n";
+    if (name == "exit")
+      break;
+
+    for (std::size_t i { 0 }; i < std::size(names); ++i) {
+      if (name == names[i]) {
+        std::cout << "Score: " << scores[i] << '\n';
+        isName = true;
+      }
+    }
+    if (!isName)
+      std::cout << "name not found\n";
+  }
+  
+  bool isScore {};
+  std::cout << "Enter scores to find names:\n"; 
+  while (std::cin >> score) {
+    isScore = false;
+
+    if (score == -1)
+      break;
+
+    for (std::size_t i { 0 }; i < std::size(scores); ++i) {
+      if (score == scores[i]) {
+        std::cout << "Name: " << names[i] << '\n';
+        isScore = true;
+      }
+    }
+    if (!isScore)
+      std::cout << "score not found\n";
   }
   return 0;
 }
